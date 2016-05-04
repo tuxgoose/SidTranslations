836b14f022e2837de6f15eaeff8650ce20ef14ae
tuxgoose <1phillip.guzman@gmail.com>
2016-05-04 10:12:39 -0500

    Simple Workflow Cycle

---
+++ WorkFlow-Cycle.ps1
@@ -0,0 +1,18 @@
+﻿Workflow Cycle{
+$list = get-content C:\Mozy\dberror.txt
+foreach -parallel($i in $list)
+{
+
+
+
+if( -Not (test-connection $i -count 1 -ErrorAction SilentlyContinue))
+{
+if(Get-WmiObject win32_computersystem)
+{
+#[string[]]$computers += $i
+Write-Output "$i" | Out-file -filepath "C:\Mozy\Online.txt" -append
+}
+}
+}
+} 
+cycle
