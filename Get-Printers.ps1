52fb76d1efc243db54dac4ecb29bd9238e62642a
tuxgoose <1phillip.guzman@gmail.com>
2016-05-04 09:59:34 -0500

    Finds a given users printers

---
+++ Get-Printers.ps1
@@ -0,0 +1,22 @@
+﻿[CmdLetBinding()]
+    Param(
+    [Parameter(Mandatory=$True,HelpMessage="Please Enter User Name(s)")]
+              [string]$UserName,
+          
+
+    [Parameter(Mandatory=$True,HelpMessage="Please Enter ComputerName(s)")]
+              [string]$ComputerName
+          )
+
+
+$drive=New-PSDrive -Name HKU   -PSProvider Registry -Root Registry::HKEY_USERS
+cd HKU:
+[string[]]$Names=(Get-ChildItem C:\Users).name
+
+$objUser = New-Object System.Security.Principal.NTAccount($Username)
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+$SIDTranslation=$strSID.Value
+
+
+Get-ChildItem Registry::\HKEY_Users\$SIDTranslation\network | Get-ItemProperty -Name Remotepath | ft remotepath, PSChildName
+Get-ChildItem Registry::\HKEY_Users\$SIDTranslation\Printers\Connections 
---
+++ Remote-Drives.ps1
@@ -0,0 +1,21 @@
+﻿
+
+[CmdLetBinding()]
+    Param(
+    [Parameter(Mandatory=$True,HelpMessage="Please Enter User Name(s)")]
+              [string]$UserName,
+          
+
+    [Parameter(Mandatory=$True,HelpMessage="Please Enter ComputerName(s)")]
+              [string]$ComputerName
+          )
+
+
+$UserName1=$UserName
+$objUser = New-Object System.Security.Principal.NTAccount($Username)
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+$SIDTranslation=$strSID.Value
+
+
+
+Invoke-Command -scriptblock  ([ScriptBlock]::Create("Get-ChildItem Registry::\HKEY_Users\$SIDTranslation\network | Get-ItemProperty -Name Remotepath | ft remotepath, PSChildName")) -ArgumentList $SIDTranslation -ComputerName $ComputerName
